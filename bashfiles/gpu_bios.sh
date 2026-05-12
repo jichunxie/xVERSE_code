b8a0416c2ddc6fecc2beaeb3a8cb617b336f067c
Xiaohui <xiaohui@Xiaohuidebijibendiannao-293.local>
2026-05-12 03:02:55 -0400

    edit

--- bashfiles/gpu_bios.sh
+++ bashfiles/gpu_bios.sh
@@ -19,7 +19,7 @@ export PYTHONUNBUFFERED=1
 
 COMPILED_ROOT="/hpc/group/xielab/xj58/xVerseAtlas/compiled_train_v1_all"
 CELLTYPE_CSV="/hpc/group/xielab/xj58/sparest_code/standard_type/cellxgene_cell_type_mapped.csv"
-RESULT_DIR="/hpc/group/xielab/xj58/pretrain_model_celltype/gmmvae_all_tissue0511_nb_smallcontrast"
+RESULT_DIR="/hpc/group/xielab/xj58/pretrain_model_celltype/gmmvae_all_tissue0511_nb_smallcontrast2"
 
 NPROC_PER_NODE=$(python - <<'PY'
 import torch
@@ -80,7 +80,7 @@ stdbuf -oL -eL "${RUN_CMD[@]}" \
   --mask-aug-policy simple \
   --mask-aug-min-frac 0.1 \
   --mask-aug-max-frac 0.5 \
-  --lambda-celltype-cls 0.1 \
+  --lambda-celltype-cls 1 \
   --lambda-contrast 0.1 \
   --lambda-real-recon 0.1 \
   --contrast-temp 0.1
