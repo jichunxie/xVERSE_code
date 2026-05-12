6ca1ccba4de107bbbf25f280da03a2ac379bbb85
Xiaohui <xiaohui@Xiaohuidebijibendiannao-293.local>
2026-05-12 03:44:43 -0400

    edit

--- bashfiles/gpu_bios.sh
+++ bashfiles/gpu_bios.sh
@@ -80,10 +80,10 @@ stdbuf -oL -eL "${RUN_CMD[@]}" \
   --mask-aug-policy simple \
   --mask-aug-min-frac 0.1 \
   --mask-aug-max-frac 0.5 \
-  --lambda-celltype-cls 1 \
-  --lambda-contrast 1 \
+  --lambda-celltype-cls 0.1 \
+  --lambda-contrast 0.1 \
   --lambda-real-recon 0.1 \
-  --lambda-prior-pi-balance 0 \
-  --lambda-prior-mu-spread 0 \
+  --lambda-prior-pi-balance 0.01 \
+  --lambda-prior-mu-spread 0.01 \
   --prior-mu-spread-tau 1.0 \
   --contrast-temp 0.1
