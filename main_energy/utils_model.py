d5351b5296cb23f6699fe6e60a5269b0bc55818d
Xiaohui <xiaohui@Xiaohuidebijibendiannao-293.local>
2026-05-13 23:41:15 -0400

    edit

--- main_energy/utils_model.py
+++ main_energy/utils_model.py
@@ -6,7 +6,7 @@ import hashlib
 import json
 import bisect
 from collections import OrderedDict, defaultdict
-from typing import Dict, List, Optional
+from typing import Dict, List, Optional, Tuple
 
 import numpy as np
 import pandas as pd
