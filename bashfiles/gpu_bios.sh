6c24db6afaec6c455128fa09667a1e3066898e78
Xiaohui <xiaohui@Xiaohuidebijibendiannao-293.local>
2026-05-15 15:41:10 -0400

    edit

--- bashfiles/gpu_bios.sh
+++ bashfiles/gpu_bios.sh
@@ -19,7 +19,7 @@ export PYTHONUNBUFFERED=1
 
 COMPILED_ROOT="/hpc/group/xielab/xj58/xVerseAtlas/compiled_train_v1_all"
 CELLTYPE_CSV="/hpc/group/xielab/xj58/sparest_code/standard_type/cellxgene_cell_type_mapped.csv"
-RESULT_DIR="/hpc/group/xielab/xj58/pretrain_model_celltype/gmmvae_all_tissue0515_poisson"
+RESULT_DIR="/hpc/group/xielab/xj58/pretrain_model_celltype/gmmvae_all_tissue0515_nb"
 
 NPROC_PER_NODE=$(python - <<'PY'
 import torch
