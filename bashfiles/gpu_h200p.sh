a669018132b5af18b657574aa3b777ddbc769ebe
Xiaohui <xiaohui@Xiaohuidebijibendiannao-293.local>
2026-05-15 16:04:43 -0400

    edit

--- bashfiles/gpu_h200p.sh
+++ bashfiles/gpu_h200p.sh
@@ -11,6 +11,7 @@
 #SBATCH --mail-user=xj58@duke.edu                 
 #SBATCH --mail-type=BEGIN,END,FAIL  
 #SBATCH --exclude=dcc-h200-gpu-05                 
+
 source ~/.bashrc
 conda activate SpaRest
 
@@ -19,7 +20,7 @@ export PYTHONUNBUFFERED=1
 
 COMPILED_ROOT="/hpc/group/xielab/xj58/xVerseAtlas/compiled_train_v1_all"
 CELLTYPE_CSV="/hpc/group/xielab/xj58/sparest_code/standard_type/cellxgene_cell_type_mapped.csv"
-RESULT_DIR="/hpc/group/xielab/xj58/pretrain_model_celltype/gmmvae_all_tissue0514_tissue_cond_largec"
+RESULT_DIR="/hpc/group/xielab/xj58/pretrain_model_celltype/gmmvae_all_tissue0515_nb"
 
 NPROC_PER_NODE=$(python - <<'PY'
 import torch
@@ -54,15 +55,15 @@ stdbuf -oL -eL "${RUN_CMD[@]}" \
   --sampler-shard-reorder-window 4096 \
   --cell-type-csv "${CELLTYPE_CSV}" \
   --result-dir "${RESULT_DIR}" \
-  --num-epochs 100 \
-  --val-every 5 \
+  --num-epochs 200 \
+  --val-every 8 \
   --batch-size 256 \
   --val-batch-size 1024 \
   --num-workers 8 \
   --val-num-workers 5 \
   --prefetch-factor 8 \
   --samples-per-id 500 \
-  --lr 1e-4 \
+  --lr 1e-3 \
   --weight-decay 1e-5 \
   --prior-type gmm \
   --recon-loss nb \
@@ -71,11 +72,13 @@ stdbuf -oL -eL "${RUN_CMD[@]}" \
   --prior-cov-rank 0 \
   --posterior-cov-rank 0 \
   --batch-emb-dim 32 \
-  --tissue-emb-dim 32 \
+  --use-batch-condition \
+  --no-use-tissue-condition \
+  --tissue-emb-dim 0 \
   --batch-cond-drop-prob 0.0 \
   --lambda-batchless-recon 0.01 \
-  --lambda-tissueless-recon 0.01 \
-  --prior-logvar-max 1 \
+  --lambda-tissueless-recon 0 \
+  --prior-logvar-max 0 \
   --expr-hidden-dim 512 \
   --mask-hidden-dim 512 \
   --dec-hidden-dim 512 \
@@ -86,14 +89,16 @@ stdbuf -oL -eL "${RUN_CMD[@]}" \
   --mask-aug-min-frac 0.1 \
   --mask-aug-max-frac 0.5 \
   --lambda-celltype-cls 1 \
-  --lambda-contrast 20 \
+  --lambda-contrast 50 \
   --contrast-embedding encoder_hidden \
   --lambda-celltype-contrast 0 \
   --celltype-contrast-temp 0.1 \
   --lambda-real-recon 0.01 \
   --lambda-prior-pi-balance 0 \
   --lambda-prior-mu-spread 0 \
-  --prior-mu-spread-tau 1.0 \
+  --prior-mu-spread-tau 0.5 \
+  --lambda-prior-logvar-l2 0 \
+  --prior-logvar-target -1 \
   --lambda-post-c-balance 0 \
   --prior-refresh-every 0 \
   --recon-gene-weight-mode cv_ema \
@@ -104,7 +109,7 @@ stdbuf -oL -eL "${RUN_CMD[@]}" \
   --recon-cell-weight-alpha 0.5 \
   --recon-cell-weight-clusters 32 \
   --recon-cell-weight-kmeans-iters 5 \
-  --lambda-rank-recon 0.05 \
+  --lambda-rank-recon 0 \
   --rank-recon-gene-pairs 256 \
   --rank-recon-cell-pairs 256 \
   --contrast-temp 0.1
