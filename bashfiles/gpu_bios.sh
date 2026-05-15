f082d67341b903ed8bc3975f971d42ce9237f131
Xiaohui <xiaohui@Xiaohuidebijibendiannao-293.local>
2026-05-14 21:48:30 -0400

    edit

--- bashfiles/gpu_bios.sh
+++ bashfiles/gpu_bios.sh
@@ -19,7 +19,7 @@ export PYTHONUNBUFFERED=1
 
 COMPILED_ROOT="/hpc/group/xielab/xj58/xVerseAtlas/compiled_train_v1_all"
 CELLTYPE_CSV="/hpc/group/xielab/xj58/sparest_code/standard_type/cellxgene_cell_type_mapped.csv"
-RESULT_DIR="/hpc/group/xielab/xj58/pretrain_model_celltype/gmmvae_all_tissue0514_ct"
+RESULT_DIR="/hpc/group/xielab/xj58/pretrain_model_celltype/gmmvae_all_tissue0514_tissue_cond_largec"
 
 NPROC_PER_NODE=$(python - <<'PY'
 import torch
@@ -62,11 +62,11 @@ stdbuf -oL -eL "${RUN_CMD[@]}" \
   --val-num-workers 5 \
   --prefetch-factor 8 \
   --samples-per-id 500 \
-  --lr 5e-4 \
+  --lr 1e-4 \
   --weight-decay 1e-5 \
   --prior-type gmm \
   --recon-loss nb \
-  --latent-dim 128 \
+  --latent-dim 256 \
   --num-components 64 \
   --prior-cov-rank 0 \
   --posterior-cov-rank 0 \
@@ -86,9 +86,9 @@ stdbuf -oL -eL "${RUN_CMD[@]}" \
   --mask-aug-min-frac 0.1 \
   --mask-aug-max-frac 0.5 \
   --lambda-celltype-cls 1 \
-  --lambda-contrast 0.5 \
+  --lambda-contrast 100 \
   --contrast-embedding encoder_hidden \
-  --lambda-celltype-contrast 0.05 \
+  --lambda-celltype-contrast 0 \
   --celltype-contrast-temp 0.1 \
   --lambda-real-recon 0.01 \
   --lambda-prior-pi-balance 0 \
