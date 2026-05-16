086dd99404dce6d31d5eccb69ae4c9feefa32007
Xiaohui <xiaohui@Xiaohuidebijibendiannao-293.local>
2026-05-16 16:17:19 -0400

    edit

--- bashfiles/gpu_bios_mfa.sh
+++ bashfiles/gpu_bios_mfa.sh
@@ -19,7 +19,7 @@ export PYTHONUNBUFFERED=1
 
 COMPILED_ROOT="/hpc/group/xielab/xj58/xVerseAtlas/compiled_train_v1_all"
 CELLTYPE_CSV="/hpc/group/xielab/xj58/sparest_code/standard_type/cellxgene_cell_type_mapped.csv"
-RESULT_DIR="/hpc/group/xielab/xj58/pretrain_model_celltype/mfa_all_tissue0513_rank6"
+RESULT_DIR="/hpc/group/xielab/xj58/pretrain_model_celltype/mfa_all_tissue0516_kl00001_con5_g256"
 
 NPROC_PER_NODE=$(python - <<'PY'
 import torch
@@ -68,8 +68,8 @@ stdbuf -oL -eL "${RUN_CMD[@]}" \
   --prior-type gmm \
   --recon-loss nb \
   --latent-dim 128 \
-  --num-components 64 \
-  --prior-cov-rank 6 \
+  --num-components 256 \
+  --prior-cov-rank 8 \
   --batch-emb-dim 32 \
   --batch-cond-drop-prob 0.0 \
   --lambda-batchless-recon 0.1 \
@@ -77,7 +77,7 @@ stdbuf -oL -eL "${RUN_CMD[@]}" \
   --expr-hidden-dim 512 \
   --mask-hidden-dim 512 \
   --dec-hidden-dim 512 \
-  --beta-kl 0.001 \
+  --beta-kl 1e-4 \
   --recon-observed-only \
   --mask-aug-prob 1.0 \
   --mask-aug-policy simple \
