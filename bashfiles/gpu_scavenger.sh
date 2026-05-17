9d3bf5bd1a97c249fd479f252f390526b6e9d80c
Xiaohui <xiaohui@Xiaohuidebijibendiannao-293.local>
2026-05-17 15:27:55 -0400

    edit

--- bashfiles/gpu_scavenger.sh
+++ bashfiles/gpu_scavenger.sh
@@ -68,7 +68,7 @@ run_one_model () {
       --max-cells 20000
 }
 
-run_one_model "mfa" "/hpc/group/xielab/xj58/pretrain_model_celltype/mfa_all_tissue0517_kl1e-5_con2_64x16_nobatch" "main_mfa" "encoder_hidden" "last_model.pth"
+run_one_model "mfa" "/hpc/group/xielab/xj58/pretrain_model_celltype/mfa_all_tissue0517_kl5e-5_con2_64x16_nobatch_nobalance" "main_mfa" "encoder_hidden" "last_model.pth"
 # run_one_model "gene_theta_encoder_0515" "/hpc/group/xielab/xj58/pretrain_model_celltype/gmmvae_all_tissue0515_2" "main_energy" "encoder_hidden"
 # run_one_model "mfa_rank4" "/hpc/group/xielab/xj58/pretrain_model_celltype/mfa_all_tissue0513_rank4" "main_mfa"
 # run_one_model "nb" "/hpc/group/xielab/xj58/pretrain_model_celltype/gmmvae_all_tissue0512" "main_energy"
