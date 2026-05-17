81de11e34da139a02cc9f1bcc018db90bb1ffbb4
Xiaohui <xiaohui@Xiaohuidebijibendiannao-293.local>
2026-05-16 23:35:39 -0400

    edit

--- bashfiles/gpu_bios_mfa.sh
+++ bashfiles/gpu_bios_mfa.sh
@@ -19,7 +19,7 @@ export PYTHONUNBUFFERED=1
 
 COMPILED_ROOT="/hpc/group/xielab/xj58/xVerseAtlas/compiled_train_v1_all"
 CELLTYPE_CSV="/hpc/group/xielab/xj58/sparest_code/standard_type/cellxgene_cell_type_mapped.csv"
-RESULT_DIR="/hpc/group/xielab/xj58/pretrain_model_celltype/mfa_all_tissue0516_kl0001_con10_g64_prior"
+RESULT_DIR="/hpc/group/xielab/xj58/pretrain_model_celltype/mfa_all_tissue0516_kl0001_con2_g16"
 
 NPROC_PER_NODE=$(python - <<'PY'
 import torch
@@ -68,7 +68,7 @@ stdbuf -oL -eL "${RUN_CMD[@]}" \
   --prior-type gmm \
   --recon-loss nb \
   --latent-dim 128 \
-  --num-components 64 \
+  --num-components 16 \
   --prior-cov-rank 8 \
   --batch-emb-dim 32 \
   --batch-cond-drop-prob 0.0 \
@@ -84,12 +84,12 @@ stdbuf -oL -eL "${RUN_CMD[@]}" \
   --mask-aug-min-frac 0.1 \
   --mask-aug-max-frac 0.5 \
   --lambda-celltype-cls 1 \
-  --lambda-contrast 10.0 \
+  --lambda-contrast 2.0 \
   --lambda-real-recon 0.1 \
-  --lambda-prior-pi-balance 1e-3 \
-  --lambda-prior-mu-spread 1e-3 \
+  --lambda-prior-pi-balance 0 \
+  --lambda-prior-mu-spread 0 \
   --prior-mu-spread-tau 1.0 \
-  --lambda-prior-logvar-l2 1e-4 \
+  --lambda-prior-logvar-l2 0 \
   --prior-logvar-target -0.5 \
   --lambda-post-c-balance 0 \
   --recon-gene-weight-mode cv_ema \
