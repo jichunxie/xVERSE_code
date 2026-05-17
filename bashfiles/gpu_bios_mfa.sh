95190c6b36d28bf9ac645535e437ef91f763e527
Xiaohui <xiaohui@Xiaohuidebijibendiannao-293.local>
2026-05-17 00:16:59 -0400

    edit

--- bashfiles/gpu_bios_mfa.sh
+++ bashfiles/gpu_bios_mfa.sh
@@ -19,7 +19,7 @@ export PYTHONUNBUFFERED=1
 
 COMPILED_ROOT="/hpc/group/xielab/xj58/xVerseAtlas/compiled_train_v1_all"
 CELLTYPE_CSV="/hpc/group/xielab/xj58/sparest_code/standard_type/cellxgene_cell_type_mapped.csv"
-RESULT_DIR="/hpc/group/xielab/xj58/pretrain_model_celltype/mfa_all_tissue0516_kl0001_con2_g64_balance"
+RESULT_DIR="/hpc/group/xielab/xj58/pretrain_model_celltype/mfa_all_tissue0517_kl0001_con2_g64_balance"
 
 NPROC_PER_NODE=$(python - <<'PY'
 import torch
