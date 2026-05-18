7e411b5ab6dc891aa9a01e86a7bd8caa07bc55fd
Xiaohui <xiaohui@Xiaohuidebijibendiannao-293.local>
2026-05-18 01:10:11 -0400

    edit

--- bashfiles/gpu_bios_mfa.sh
+++ bashfiles/gpu_bios_mfa.sh
@@ -96,8 +96,8 @@ stdbuf -oL -eL "${RUN_CMD[@]}" \
   --lambda-prior-pi-balance 0 \
   --lambda-prior-mu-spread 0 \
   --prior-mu-spread-tau 0.5 \
-  --lambda-prior-logvar-l2 1e-4 \
-  --lambda-prior-factor-l2 1e-4\
+  --lambda-prior-logvar-l2 0 \
+  --lambda-prior-factor-l2 0\
   --prior-logvar-target 0 \
   --prior-init-epoch 0 \
   --prior-init-samples 500000 \
