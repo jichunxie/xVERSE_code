149926d93d19249543e2582206307c45e4e38925
Xiaohui <xiaohui@Xiaohuidebijibendiannao-293.local>
2026-05-17 23:14:47 -0400

    edit

--- bashfiles/gpu_bios_mfa.sh
+++ bashfiles/gpu_bios_mfa.sh
@@ -89,8 +89,8 @@ stdbuf -oL -eL "${RUN_CMD[@]}" \
   --mask-aug-policy simple \
   --mask-aug-min-frac 0.1 \
   --mask-aug-max-frac 0.5 \
-  --lambda-celltype-cls 1 \
-  --lambda-contrast 1.0 \
+  --lambda-celltype-cls 0.1 \
+  --lambda-contrast 0.1 \
   --lambda-real-recon 0.5 \
   --lambda-prior-pi-balance 0 \
   --lambda-prior-mu-spread 0 \
