771c699680a3b430a7906f42eaf117896bd2e622
Xiaohui <xiaohui@Xiaohuidebijibendiannao-293.local>
2026-05-18 16:26:46 -0400

    edit

--- bashfiles/gpu_bios_mfa.sh
+++ bashfiles/gpu_bios_mfa.sh
@@ -80,7 +80,7 @@ stdbuf -oL -eL "${RUN_CMD[@]}" \
   --expr-hidden-dim 512 \
   --mask-hidden-dim 512 \
   --dec-hidden-dim 512 \
-  --beta-kl 1e-4 \
+  --beta-kl 3e-4 \
   --beta-u-kl-multiplier 1.0 \
   --beta-eps-kl-multiplier 1.0 \
   --beta-kl-warmup-epochs 10 \
