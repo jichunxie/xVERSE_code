c704518052f6d3dbad117b31a7db0c5fb7d6e656
Xiaohui <xiaohui@Xiaohuidebijibendiannao-293.local>
2026-05-17 20:04:13 -0400

    edit

--- bashfiles/gpu_bios_mfa.sh
+++ bashfiles/gpu_bios_mfa.sh
@@ -19,7 +19,7 @@ export PYTHONUNBUFFERED=1
 
 COMPILED_ROOT="/hpc/group/xielab/xj58/xVerseAtlas/compiled_train_v1_all"
 CELLTYPE_CSV="/hpc/group/xielab/xj58/sparest_code/standard_type/cellxgene_cell_type_mapped.csv"
-RESULT_DIR="/hpc/group/xielab/xj58/pretrain_model_celltype/mfa_all_tissue0517_con2_64x16_nobatch_nobalance_kl1e-4"
+RESULT_DIR="/hpc/group/xielab/xj58/pretrain_model_celltype/mfa_all_tissue0517_con2_8x4_nobatch_nobalance_kl1e-4"
 
 NPROC_PER_NODE=$(python - <<'PY'
 import torch
@@ -68,8 +68,8 @@ stdbuf -oL -eL "${RUN_CMD[@]}" \
   --prior-type gmm \
   --recon-loss nb \
   --latent-dim 128 \
-  --num-components 64 \
-  --prior-cov-rank 16 \
+  --num-components 8 \
+  --prior-cov-rank 4 \
   --prior-mu-init grouped_sphere \
   --prior-mu-init-radius 5 \
   --prior-mu-init-groups 8 \
@@ -78,7 +78,7 @@ stdbuf -oL -eL "${RUN_CMD[@]}" \
   --batch-cond-drop-prob 0.0 \
   --lambda-batchless-recon 0 \
   --prior-logvar-min -4 \
-  --prior-logvar-max 0 \
+  --prior-logvar-max 4 \
   --expr-hidden-dim 512 \
   --mask-hidden-dim 512 \
   --dec-hidden-dim 512 \
@@ -91,8 +91,8 @@ stdbuf -oL -eL "${RUN_CMD[@]}" \
   --mask-aug-min-frac 0.1 \
   --mask-aug-max-frac 0.5 \
   --lambda-celltype-cls 1 \
-  --lambda-contrast 3.0 \
-  --lambda-real-recon 0.1 \
+  --lambda-contrast 1.0 \
+  --lambda-real-recon 0.5 \
   --lambda-prior-pi-balance 0 \
   --lambda-prior-mu-spread 0 \
   --prior-mu-spread-tau 0.5 \
