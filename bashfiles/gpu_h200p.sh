4e1e129acb8bd7c16c499d19594a2f04ab91e231
Xiaohui <xiaohui@Xiaohuidebijibendiannao-293.local>
2026-05-19 18:14:30 -0400

    edit

--- bashfiles/gpu_h200p.sh
+++ bashfiles/gpu_h200p.sh
@@ -20,7 +20,7 @@ export PYTHONUNBUFFERED=1
 
 COMPILED_ROOT="/hpc/group/xielab/xj58/xVerseAtlas/compiled_train_v1_all"
 CELLTYPE_CSV="/hpc/group/xielab/xj58/sparest_code/standard_type/cellxgene_cell_type_mapped.csv"
-RESULT_DIR="/hpc/group/xielab/xj58/pretrain_model_celltype/mfa_all_tissue0517_con2_8x4_nobatch_nobalance_kl3e-4_varfree"
+RESULT_DIR="/hpc/group/xielab/xj58/pretrain_model_celltype/mfa_all_tissue0518_128x16_bio_ct_filter_kl1e-2-norm_v2_reweight_sharetheta"
 
 NPROC_PER_NODE=$(python - <<'PY'
 import torch
@@ -57,24 +57,23 @@ stdbuf -oL -eL "${RUN_CMD[@]}" \
   --result-dir "${RESULT_DIR}" \
   --num-epochs 100 \
   --val-every 10 \
-  --batch-size 256 \
+  --batch-size 1024 \
   --val-batch-size 1024 \
   --num-workers 8 \
   --val-num-workers 8 \
   --val-persistent-workers \
   --prefetch-factor 8 \
   --samples-per-id 500 \
-  --lr 1e-4 \
+  --lr 3e-3 \
+  --prior-lr-multiplier 1 \
   --weight-decay 1e-5 \
   --prior-type gmm \
   --recon-loss nb \
   --latent-dim 128 \
-  --num-components 8 \
-  --prior-cov-rank 4 \
-  --prior-mu-init grouped_sphere \
-  --prior-mu-init-radius 5 \
-  --prior-mu-init-groups 8 \
-  --prior-mu-init-local-radius 2 \
+  --num-components 128 \
+  --prior-cov-rank 16 \
+  --prior-mu-init sphere \
+  --prior-mu-init-radius 4 \
   --batch-emb-dim 0 \
   --batch-cond-drop-prob 0.0 \
   --lambda-batchless-recon 0 \
@@ -83,23 +82,25 @@ stdbuf -oL -eL "${RUN_CMD[@]}" \
   --expr-hidden-dim 512 \
   --mask-hidden-dim 512 \
   --dec-hidden-dim 512 \
-  --beta-kl 3e-4 \
+  --beta-kl 1e-2 \
+  --beta-u-kl-multiplier 1.0 \
+  --beta-eps-kl-multiplier 1.0 \
   --beta-kl-warmup-epochs 10 \
-  --beta-kl-warmup-start 5e-5 \
+  --beta-kl-warmup-start 1e-2 \
   --recon-observed-only \
   --mask-aug-prob 1.0 \
   --mask-aug-policy simple \
-  --mask-aug-min-frac 0.1 \
+  --mask-aug-min-frac 0 \
   --mask-aug-max-frac 0.5 \
-  --lambda-celltype-cls 1 \
-  --lambda-contrast 1.0 \
+  --lambda-celltype-cls 0.5 \
+  --lambda-contrast 0.25 \
   --lambda-real-recon 0.5 \
   --lambda-prior-pi-balance 0 \
   --lambda-prior-mu-spread 0 \
-  --prior-mu-spread-tau 0.5 \
+  --prior-mu-spread-tau 0 \
   --lambda-prior-logvar-l2 0 \
-  --lambda-prior-factor-l2 0\
-  --prior-logvar-target -0.5 \
+  --lambda-prior-factor-l2 0 \
+  --prior-logvar-target 0 \
   --prior-init-epoch 0 \
   --prior-init-samples 500000 \
   --prior-init-kmeans-iters 20 \
@@ -112,9 +113,10 @@ stdbuf -oL -eL "${RUN_CMD[@]}" \
   --prior-init-factor-std 0.01 \
   --lambda-post-c-balance 0 \
   --recon-gene-weight-mode none \
-  --recon-gene-weight-alpha 0 \
+  --recon-gene-weight-alpha 0.2 \
   --recon-cell-weight-mode none \
-  --recon-cell-weight-alpha 0 \
+  --recon-cell-weight-alpha 0.5 \
   --recon-cell-weight-clusters 32 \
   --recon-cell-weight-kmeans-iters 5 \
-  --contrast-temp 0.1
+  --contrast-temp 0.1 \
+  --resume-from last
