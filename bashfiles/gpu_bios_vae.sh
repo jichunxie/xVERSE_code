9228fa7bf7be72ebf4869eb08e5ea260437a2e05
Xiaohui <xiaohui@Xiaohuidebijibendiannao-293.local>
2026-05-18 22:28:22 -0400

    edit

--- bashfiles/gpu_bios_vae.sh
+++ bashfiles/gpu_bios_vae.sh
@@ -19,7 +19,7 @@ export PYTHONUNBUFFERED=1
 
 COMPILED_ROOT="/hpc/group/xielab/xj58/xVerseAtlas/compiled_train_v1_all"
 CELLTYPE_CSV="/hpc/group/xielab/xj58/sparest_code/standard_type/cellxgene_cell_type_mapped.csv"
-RESULT_DIR="/hpc/group/xielab/xj58/pretrain_model_celltype/vae_all_tissue0517_con3_nobatch_kl1e-3_con1_reweight"
+RESULT_DIR="/hpc/group/xielab/xj58/pretrain_model_celltype/vae_all_tissue0518_con1_nobatch_kl1e-4_noct"
 
 NPROC_PER_NODE=$(python - <<'PY'
 import torch
@@ -82,13 +82,13 @@ stdbuf -oL -eL "${RUN_CMD[@]}" \
   --mask-aug-policy simple \
   --mask-aug-min-frac 0.1 \
   --mask-aug-max-frac 0.5 \
-  --lambda-celltype-cls 1 \
+  --lambda-celltype-cls 0 \
   --lambda-contrast 1.0 \
-  --lambda-real-recon 0.1 \
+  --lambda-real-recon 0.5 \
   --recon-gene-weight-mode cv_ema \
-  --recon-gene-weight-alpha 0.3 \
+  --recon-gene-weight-alpha 0 \
   --recon-cell-weight-mode batch_kmeans \
-  --recon-cell-weight-alpha 0.3 \
+  --recon-cell-weight-alpha 0 \
   --recon-cell-weight-clusters 32 \
   --recon-cell-weight-kmeans-iters 5 \
   --contrast-temp 0.1
