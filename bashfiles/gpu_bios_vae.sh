be26262eb63a377ecaaf602d8f0a402a7977947b
Xiaohui <xiaohui@Xiaohuidebijibendiannao-293.local>
2026-05-19 02:47:35 -0400

    edit

--- bashfiles/gpu_bios_vae.sh
+++ bashfiles/gpu_bios_vae.sh
@@ -19,7 +19,7 @@ export PYTHONUNBUFFERED=1
 
 COMPILED_ROOT="/hpc/group/xielab/xj58/xVerseAtlas/compiled_train_v1_all"
 CELLTYPE_CSV="/hpc/group/xielab/xj58/sparest_code/standard_type/cellxgene_cell_type_mapped.csv"
-RESULT_DIR="/hpc/group/xielab/xj58/pretrain_model_celltype/vae_all_tissue0518_con1_nobatch_kl1e-4_ct"
+RESULT_DIR="/hpc/group/xielab/xj58/pretrain_model_celltype/vae_all_tissue0518_con1_nobatch_kl1e-4_ct_reweight"
 
 NPROC_PER_NODE=$(python - <<'PY'
 import torch
@@ -88,7 +88,7 @@ stdbuf -oL -eL "${RUN_CMD[@]}" \
   --recon-gene-weight-mode cv_ema \
   --recon-gene-weight-alpha 0 \
   --recon-cell-weight-mode batch_kmeans \
-  --recon-cell-weight-alpha 0 \
+  --recon-cell-weight-alpha 0.5 \
   --recon-cell-weight-clusters 32 \
   --recon-cell-weight-kmeans-iters 5 \
   --contrast-temp 0.1
