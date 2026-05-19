237931675c7ae1bcf5f328896abd3ec8def0d11b
Xiaohui <xiaohui@Xiaohuidebijibendiannao-293.local>
2026-05-18 22:38:11 -0400

    edit

--- bashfiles/gpu_bios_mfa.sh
+++ bashfiles/gpu_bios_mfa.sh
@@ -19,7 +19,7 @@ export PYTHONUNBUFFERED=1
 
 COMPILED_ROOT="/hpc/group/xielab/xj58/xVerseAtlas/compiled_train_v1_all"
 CELLTYPE_CSV="/hpc/group/xielab/xj58/sparest_code/standard_type/cellxgene_cell_type_mapped.csv"
-RESULT_DIR="/hpc/group/xielab/xj58/pretrain_model_celltype/mfa_all_tissue0518_128x4_bio_noct_filter_kl1e-4_priorlr0.1"
+RESULT_DIR="/hpc/group/xielab/xj58/pretrain_model_celltype/mfa_all_tissue0518_128x4_bio_noct_filter_kl1e-4_priorlr0.1_push"
 
 NPROC_PER_NODE=$(python - <<'PY'
 import torch
@@ -95,10 +95,10 @@ stdbuf -oL -eL "${RUN_CMD[@]}" \
   --lambda-contrast 1 \
   --lambda-real-recon 0.5 \
   --lambda-prior-pi-balance 0 \
-  --lambda-prior-mu-spread 0 \
+  --lambda-prior-mu-spread 0.1 \
   --prior-mu-spread-tau 0.5 \
-  --lambda-prior-logvar-l2 0 \
-  --lambda-prior-factor-l2 0 \
+  --lambda-prior-logvar-l2 0.1 \
+  --lambda-prior-factor-l2 0.1 \
   --prior-logvar-target 0 \
   --prior-init-epoch 0 \
   --prior-init-samples 500000 \
