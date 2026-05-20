15b8558b7e66789d783f4fb1d7b47c23b2c89acc
Xiaohui <xiaohui@Xiaohuidebijibendiannao-293.local>
2026-05-20 00:13:16 -0400

    edit

--- bashfiles/gpu_scavenger_mfa.sh
+++ bashfiles/gpu_scavenger_mfa.sh
@@ -19,7 +19,7 @@ export PYTHONUNBUFFERED=1
 
 COMPILED_ROOT="/hpc/group/xielab/xj58/xVerseAtlas/compiled_train_v1_all"
 CELLTYPE_CSV="/hpc/group/xielab/xj58/sparest_code/standard_type/cellxgene_cell_type_mapped.csv"
-RESULT_DIR="/hpc/group/xielab/xj58/pretrain_model_celltype/mfa_all_tissue0518_128x16_bio_ct_filter_kl2e-2-norm_v2_reweight_sharetheta_meta"
+RESULT_DIR="/hpc/group/xielab/xj58/pretrain_model_celltype/mfa_all_tissue0518_128x16_bio_ct_filter_kl5e-2-norm_v2_reweight_sharetheta_meta"
 
 NPROC_PER_NODE=$(python - <<'PY'
 import torch
@@ -81,11 +81,11 @@ stdbuf -oL -eL "${RUN_CMD[@]}" \
   --expr-hidden-dim 512 \
   --mask-hidden-dim 512 \
   --dec-hidden-dim 512 \
-  --beta-kl 2e-2 \
+  --beta-kl 5e-2 \
   --beta-u-kl-multiplier 1.0 \
   --beta-eps-kl-multiplier 1.0 \
   --beta-kl-warmup-epochs 10 \
-  --beta-kl-warmup-start 2e-2 \
+  --beta-kl-warmup-start 5e-2 \
   --recon-observed-only \
   --mask-aug-prob 1.0 \
   --mask-aug-policy simple \
