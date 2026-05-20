785a06e7a8c165c261dcc94b8c9ff6d8bc676765
Xiaohui <xiaohui@Xiaohuidebijibendiannao-293.local>
2026-05-20 03:31:04 -0400

    edit

--- bashfiles/gpu_bios_mfa.sh
+++ bashfiles/gpu_bios_mfa.sh
@@ -19,7 +19,7 @@ export PYTHONUNBUFFERED=1
 
 COMPILED_ROOT="/hpc/group/xielab/xj58/xVerseAtlas/compiled_train_v1_all"
 CELLTYPE_CSV="/hpc/group/xielab/xj58/sparest_code/standard_type/cellxgene_cell_type_mapped.csv"
-RESULT_DIR="/hpc/group/xielab/xj58/pretrain_model_celltype/mfa_all_tissue0518_128x16_bio_ct_filter_kl2e-2-norm_v2_reweight_sharetheta_meta_withbatch"
+RESULT_DIR="/hpc/group/xielab/xj58/pretrain_model_celltype/mfa_all_tissue0520_high"
 
 NPROC_PER_NODE=$(python - <<'PY'
 import torch
@@ -54,7 +54,7 @@ stdbuf -oL -eL "${RUN_CMD[@]}" \
   --sampler-shard-reorder-window 4096 \
   --cell-type-csv "${CELLTYPE_CSV}" \
   --result-dir "${RESULT_DIR}" \
-  --num-epochs 100 \
+  --num-epochs 200 \
   --val-every 10 \
   --batch-size 1024 \
   --val-batch-size 1024 \
@@ -69,30 +69,30 @@ stdbuf -oL -eL "${RUN_CMD[@]}" \
   --prior-type gmm \
   --recon-loss nb \
   --latent-dim 128 \
-  --num-components 128 \
+  --num-components 256 \
   --prior-cov-rank 16 \
   --prior-mu-init sphere \
-  --prior-mu-init-radius 2 \
-  --batch-emb-dim 32 \
+  --prior-mu-init-radius 4 \
+  --batch-emb-dim 0 \
   --batch-cond-drop-prob 0.0 \
-  --lambda-batchless-recon 1 \
+  --lambda-batchless-recon 0 \
   --prior-logvar-min -4 \
   --prior-logvar-max 4 \
   --expr-hidden-dim 512 \
   --mask-hidden-dim 512 \
   --dec-hidden-dim 512 \
-  --beta-kl 2e-2 \
+  --beta-kl 5e-2 \
   --beta-u-kl-multiplier 1.0 \
   --beta-eps-kl-multiplier 1.0 \
   --beta-kl-warmup-epochs 10 \
-  --beta-kl-warmup-start 2e-2 \
+  --beta-kl-warmup-start 5e-2 \
   --recon-observed-only \
   --mask-aug-prob 1.0 \
   --mask-aug-policy simple \
   --mask-aug-min-frac 0 \
   --mask-aug-max-frac 0.5 \
-  --lambda-celltype-cls 0.5 \
-  --lambda-contrast 0.25 \
+  --lambda-celltype-cls 5 \
+  --lambda-contrast 2 \
   --lambda-real-recon 0.5 \
   --lambda-prior-pi-balance 0 \
   --lambda-prior-mu-spread 0 \
