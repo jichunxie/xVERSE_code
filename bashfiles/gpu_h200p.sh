f6c2c46464e3753787563867f1c82743414e634d
Xiaohui <xiaohui@Xiaohuidebijibendiannao-293.local>
2026-05-21 04:59:05 -0400

    edit

--- bashfiles/gpu_h200p.sh
+++ bashfiles/gpu_h200p.sh
@@ -68,7 +68,7 @@ stdbuf -oL -eL "${RUN_CMD[@]}" \
   --prefetch-factor 8 \
   --samples-per-id 500 \
   --lr 1e-3 \
-  --prior-lr-multiplier 1 \
+  --prior-lr-multiplier 0.05 \
   --weight-decay 1e-5 \
   --prior-type gmm \
   --recon-loss nb \
