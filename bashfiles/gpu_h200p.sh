ceeb3a45e8325a754c2057553cf5282e45e399b1
Xiaohui <xiaohui@Xiaohuidebijibendiannao-293.local>
2026-05-21 18:46:57 -0400

    edit

--- bashfiles/gpu_h200p.sh
+++ bashfiles/gpu_h200p.sh
@@ -21,7 +21,7 @@ export PYTHONUNBUFFERED=1
 COMPILED_ROOT="/hpc/group/xielab/xj58/xVerseAtlas/compiled_train_v1_all"
 CELLTYPE_CSV="/hpc/group/xielab/xj58/general/cellxgene_cell_type_id2name.csv"
 CELLTYPE_TEXT_EMB="/hpc/group/xielab/xj58/general/cellxgene_cell_type_text_embeddings.npz"
-RESULT_DIR="/hpc/group/xielab/xj58/pretrain_model_celltype/mfa_all_tissue0521_celltype_text_128x8x015_reweight_con"
+RESULT_DIR="/hpc/group/xielab/xj58/pretrain_model_celltype/mfa_all_tissue0521_celltype_text_128x8x015_reweight_con2"
 
 NPROC_PER_NODE=$(python - <<'PY'
 import torch
@@ -87,11 +87,11 @@ stdbuf -oL -eL "${RUN_CMD[@]}" \
   --expr-hidden-dim 512 \
   --mask-hidden-dim 512 \
   --dec-hidden-dim 512 \
-  --beta-kl 3e-2 \
+  --beta-kl 4e-2 \
   --beta-u-kl-multiplier 1.0 \
   --beta-eps-kl-multiplier 1.0 \
   --beta-kl-warmup-epochs 2 \
-  --beta-kl-warmup-start 3e-2 \
+  --beta-kl-warmup-start 4e-2 \
   --recon-observed-only \
   --mask-aug-prob 1.0 \
   --mask-aug-policy simple \
@@ -132,3 +132,4 @@ stdbuf -oL -eL "${RUN_CMD[@]}" \
   --prior-lr-multiplier 1 \
   --decoder-lr-multiplier 1 \
   --recon-head-lr-multiplier 1 \
+  --log-every 200 \
