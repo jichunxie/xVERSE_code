db07054f7b4f3faf679c9bed9fac4239c985c55d
Xiaohui <xiaohui@Xiaohuidebijibendiannao-293.local>
2026-05-21 13:32:44 -0400

    edit

--- bashfiles/gpu_h200p.sh
+++ bashfiles/gpu_h200p.sh
@@ -21,7 +21,7 @@ export PYTHONUNBUFFERED=1
 COMPILED_ROOT="/hpc/group/xielab/xj58/xVerseAtlas/compiled_train_v1_all"
 CELLTYPE_CSV="/hpc/group/xielab/xj58/general/cellxgene_cell_type_id2name.csv"
 CELLTYPE_TEXT_EMB="/hpc/group/xielab/xj58/general/cellxgene_cell_type_text_embeddings.npz"
-RESULT_DIR="/hpc/group/xielab/xj58/pretrain_model_celltype/mfa_all_tissue0521_celltype_text_128x8x02_dlr"
+RESULT_DIR="/hpc/group/xielab/xj58/pretrain_model_celltype/mfa_all_tissue0521_celltype_text_32x8x02"
 
 NPROC_PER_NODE=$(python - <<'PY'
 import torch
@@ -73,7 +73,7 @@ stdbuf -oL -eL "${RUN_CMD[@]}" \
   --prior-type gmm \
   --recon-loss nb \
   --latent-dim 128 \
-  --num-components 128 \
+  --num-components 32 \
   --prior-cov-rank 8 \
   --prior-mu-init sphere \
   --prior-mu-init-radius 4 \
@@ -82,9 +82,9 @@ stdbuf -oL -eL "${RUN_CMD[@]}" \
   --lambda-batchless-recon 0 \
   --prior-logvar-min -4 \
   --prior-logvar-max 4 \
-  --expr-hidden-dim 512 \
-  --mask-hidden-dim 512 \
-  --dec-hidden-dim 512 \
+  --expr-hidden-dim 256 \
+  --mask-hidden-dim 256 \
+  --dec-hidden-dim 128 \
   --beta-kl 5e-2 \
   --beta-u-kl-multiplier 1.0 \
   --beta-eps-kl-multiplier 1.0 \
@@ -95,10 +95,10 @@ stdbuf -oL -eL "${RUN_CMD[@]}" \
   --mask-aug-policy simple \
   --mask-aug-min-frac 0 \
   --mask-aug-max-frac 0.5 \
-  --lambda-celltype-cls 1 \
+  --lambda-celltype-cls 2 \
   --celltype-text-embedding-path "${CELLTYPE_TEXT_EMB}" \
-  --celltype-text-temp 0.2 \
-  --lambda-contrast 1 \
+  --celltype-text-temp 0.15 \
+  --lambda-contrast 1.5 \
   --lambda-real-recon 0.5 \
   --lambda-prior-pi-balance 0 \
   --lambda-prior-mu-spread 0 \
@@ -120,12 +120,12 @@ stdbuf -oL -eL "${RUN_CMD[@]}" \
   --recon-gene-weight-mode none \
   --recon-gene-weight-alpha 0 \
   --recon-cell-weight-mode none \
-  --recon-cell-weight-alpha 0.5 \
+  --recon-cell-weight-alpha 0 \
   --recon-cell-weight-clusters 32 \
   --recon-cell-weight-kmeans-iters 5 \
   --contrast-temp 0.1 \
   --resume-from last \
-  --encoder-lr-multiplier 2 \
-  --prior-lr-multiplier 2 \
+  --encoder-lr-multiplier 1 \
+  --prior-lr-multiplier 1 \
   --decoder-lr-multiplier 1 \
   --recon-head-lr-multiplier 1 \
