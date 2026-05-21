a2b566490eea19c7c5450ea0baf1858d36dff70c
Xiaohui <xiaohui@Xiaohuidebijibendiannao-293.local>
2026-05-21 01:01:12 -0400

    edit

--- bashfiles/gpu_bios_mfa.sh
+++ bashfiles/gpu_bios_mfa.sh
@@ -21,7 +21,7 @@ export PYTHONUNBUFFERED=1
 COMPILED_ROOT="/hpc/group/xielab/xj58/xVerseAtlas/compiled_train_v1_all"
 CELLTYPE_CSV="/hpc/group/xielab/xj58/general/cellxgene_cell_type_id2name.csv"
 CELLTYPE_TEXT_EMB="/hpc/group/xielab/xj58/general/cellxgene_cell_type_text_embeddings.npz"
-RESULT_DIR="/hpc/group/xielab/xj58/pretrain_model_celltype/mfa_all_tissue0520_celltype_text_64x8x01"
+RESULT_DIR="/hpc/group/xielab/xj58/pretrain_model_celltype/mfa_all_tissue0520_celltype_text_64x8x01_n"
 
 NPROC_PER_NODE=$(python - <<'PY'
 import torch
@@ -95,10 +95,10 @@ stdbuf -oL -eL "${RUN_CMD[@]}" \
   --mask-aug-policy simple \
   --mask-aug-min-frac 0 \
   --mask-aug-max-frac 0.5 \
-  --lambda-celltype-cls 2 \
+  --lambda-celltype-cls 1 \
   --celltype-text-embedding-path "${CELLTYPE_TEXT_EMB}" \
   --celltype-text-temp 0.1 \
-  --lambda-contrast 1 \
+  --lambda-contrast 0.5 \
   --lambda-real-recon 0.5 \
   --lambda-prior-pi-balance 0 \
   --lambda-prior-mu-spread 0 \
