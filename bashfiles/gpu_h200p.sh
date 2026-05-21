2727a15944a4fef4f2052610b552b54ca3f87c3d
Xiaohui <xiaohui@Xiaohuidebijibendiannao-293.local>
2026-05-21 05:50:50 -0400

    edit

--- bashfiles/gpu_h200p.sh
+++ bashfiles/gpu_h200p.sh
@@ -89,7 +89,7 @@ stdbuf -oL -eL "${RUN_CMD[@]}" \
   --beta-u-kl-multiplier 1.0 \
   --beta-eps-kl-multiplier 1.0 \
   --beta-kl-warmup-epochs 2 \
-  --beta-kl-warmup-start 0 \
+  --beta-kl-warmup-start 5e-2 \
   --recon-observed-only \
   --mask-aug-prob 1.0 \
   --mask-aug-policy simple \
