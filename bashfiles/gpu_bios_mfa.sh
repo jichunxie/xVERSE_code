b1bef80bcc208df5c05a45de29e066e8f47a44f7
Xiaohui <xiaohui@Xiaohuidebijibendiannao-293.local>
2026-05-23 19:38:49 -0400

    edit

--- bashfiles/gpu_bios_mfa.sh
+++ bashfiles/gpu_bios_mfa.sh
@@ -67,7 +67,7 @@ stdbuf -oL -eL "${RUN_CMD[@]}" \
   --val-persistent-workers \
   --prefetch-factor 8 \
   --samples-per-id 200 \
-  --lr 1e-3 \
+  --lr 5e-3 \
   --prior-lr-multiplier 1 \
   --epoch-lr-gamma 1 \
   --weight-decay 1e-5 \
