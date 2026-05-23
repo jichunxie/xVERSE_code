a17d5146908b9c5f0b9f5b8b45be643579b347d4
Xiaohui <xiaohui@Xiaohuidebijibendiannao-293.local>
2026-05-23 02:40:59 -0400

    edit

--- bashfiles/gpu_bios_mfa.sh
+++ bashfiles/gpu_bios_mfa.sh
@@ -20,7 +20,7 @@ export PYTHONUNBUFFERED=1
 COMPILED_ROOT="/hpc/group/xielab/xj58/xVerseAtlas/compiled_train_v1_all"
 CELLTYPE_CSV="/hpc/group/xielab/xj58/general/cellxgene_cell_type_id2name.csv"
 CELLTYPE_TEXT_EMB="/hpc/group/xielab/xj58/general/cellxgene_cell_type_text_embeddings.npz"
-RESULT_DIR="/hpc/group/xielab/xj58/pretrain_model_celltype/mfa_all_tissue0522_celltype_text_32x64_xversemask2"
+RESULT_DIR="/hpc/group/xielab/xj58/pretrain_model_celltype/mfa_all_tissue0522_celltype_text_16x64_xversemask2"
 
 NPROC_PER_NODE=$(python - <<'PY'
 import torch
@@ -74,7 +74,7 @@ stdbuf -oL -eL "${RUN_CMD[@]}" \
   --prior-type gmm \
   --recon-loss nb \
   --latent-dim 128 \
-  --num-components 32 \
+  --num-components 16 \
   --prior-cov-rank 64 \
   --prior-mu-init sphere \
   --prior-mu-init-radius 5 \
